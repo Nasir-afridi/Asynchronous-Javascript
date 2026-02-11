d0df53cb79ee5058ab746a8311f747c98ba54e2f
Nasir-afridi <nasirwc05@gmail.com>
2026-02-11 21:23:18 +0500

    added the comment

--- 01-Sync.js
+++ 01-Sync.js
@@ -1,3 +1,10 @@
+/* 
+ Synchronous : 
+ This is the normal way code runs in JavaScript.
+ Each line waits for the previous line to finish before running.
+ Think of it like following steps one by one in order.
+*/
+
 // Order tracking example for Synchronous js.
 function checkingInventory() {
   console.log("checking the inventory...");
