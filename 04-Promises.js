ce96c68a288566e45218738843b2abeec09d909e
Nasir-afridi <nasirwc05@gmail.com>
2026-02-14 00:13:33 +0500

    added .catch on every single function for error handling

--- 04-Promises.js
+++ 04-Promises.js
@@ -12,7 +12,7 @@ function createOrder() {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("Creating the Order...");
-      resolve();
+      reject(new Error("Failed To create order..."));
     }, 1000);
   });
   return promise;
@@ -22,7 +22,7 @@ function chargePayment() {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("Charging the Payment...");
-      resolve();
+      reject(new Error("Failed To charge payment..."));
     }, 1500);
   });
   return promise;
@@ -32,16 +32,31 @@ function sendInvoice() {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("Sending the invoice...");
-      resolve();
+      reject(new Error("Failed To send invoice..."));
     }, 3000);
   });
   return promise;
 }
 
 function main() {
+  /* 
+    .then isliye lagta hai takay function aik he sequence mein aatay jain orr aggr hr function ka alag alag error daikhne hai to hr hr function prr alag alag catch lagana hoga aik he catch lagaya
+    to first one error show hoga hamesha osky bad walay nahe isliye
+    alag alag catch lagana sahe hai
+*/
+
   checkingInventory()
+    .catch((err) => {
+      console.log("error is : ", err);
+    })
     .then(createOrder)
+    .catch((err) => {
+      console.log("error is : ", err);
+    })
     .then(chargePayment)
+    .catch((err) => {
+      console.log("error is : ", err);
+    })
     .then(sendInvoice)
     .catch((err) => {
       console.log("error is : ", err);
